f28b0742256b51ea823b6d609289ad4c12d49c91
ssw03270 <ttd8591@gmail.com>
2021-01-02 23:46:55 +0900

    [Fix] - Fix itemSlot_03 to skillSlot

--- RoguelikeUnity/Assets/Scripts/PlayerController.cs
+++ RoguelikeUnity/Assets/Scripts/PlayerController.cs
@@ -1,14 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
+using System.IO;
 
 public class PlayerController : MonoBehaviour
 {
     private Animator animator;
     private Camera camera;
 
-    public List<bool> skillCondition = new List<bool>();   // 스킬 습득 여부 확인 리스트
+    public List<int> skillCondition = new List<int>();      // 스킬 습득 여부 확인 리스트
     private float moveSpeed = 3f;                           // 플레이어 이동 속도
     private bool isPlayerMove;                              // 플레이어가 이동 여부 확인
     private bool isPlayerAttack;                            // 플레이어 공격 여부 확인
@@ -19,9 +21,9 @@ public class PlayerController : MonoBehaviour
     public int maxHealth;                                   // 플레이어 최대 체력
     public int maxMana;                                     // 플레이어 최대 마나
     public float delaySkill;                                // 스킬 딜레이 타임
-    private int essenceCount;                               // 정수 갯수
+    public int essenceCount;                                // 정수 갯수
 
-    public GameObject FireBall;                             // 스킬 1번 : 화염구
+    public GameObject FireBall;                             // 스킬 0번 : 화염구
     public GameObject Lightning;                            // 스킬 1번 : 번개작렬
     public SkillFireBall skillFireBall;                     // 스킬 화염구 관리 클래스
     public SkillLightning skillLightning;                   // 스킬 번개작렬 관리 클래스
@@ -36,12 +38,17 @@ public class PlayerController : MonoBehaviour
     public bool isInvincible;                               // 플레이어 무적 여부
     public float invincibleTime;                            // 남은 무적 시간
 
-    public TextMeshProUGUI itemCount_01;                    // 아이템 슬롯 01 갯수 UI
-    public TextMeshProUGUI itemCount_02;                    // 아이템 슬롯 02 갯수 UI
-    public TextMeshProUGUI itemCount_03;                    // 아이템 슬롯 03 갯수 UI
+    public TextMeshProUGUI slotText_01;                     // 아이템 슬롯 01 갯수 UI
+    public TextMeshProUGUI slotText_02;                     // 아이템 슬롯 02 갯수 UI
+    public TextMeshProUGUI slotText_03;                     // 스킬 슬롯 UI
+    public Image slotSkillImage;                             // 스킬 슬롯 이미지
 
-    private Dictionary<string, int> itemSlot = new Dictionary<string, int>();   // 아이템 슬롯에 있는 아이템 종류             
-    private Dictionary<string, int> itemCount = new Dictionary<string, int>();  // 아이템 슬롯에 있는 아이템 갯수             
+    public int healthPotionCount;                          // 체력 포션 갯수
+    public int manaPotionCount;                            // 마나 포션 갯수
+    public int currentSkillNumber;                         // 현재 등록된 스킬 번호
+
+    public int healthPotionPower;                           // 체력 포션 효과
+    public int manaPotionPower;                             // 체력 포션 효과
 
     public TextMeshProUGUI essenceCountText;                // 정수 갯수 UI 
 
@@ -61,14 +68,6 @@ public class PlayerController : MonoBehaviour
 
         spriteRenderer = GetComponent<SpriteRenderer>();
         playerColor = spriteRenderer.color;
-
-        itemSlot.Add("Z", 1);                               // 체력 포션
-        itemSlot.Add("X", 2);                               // 마나 포션
-        itemSlot.Add("C", 0);                               // 공백
-
-        itemCount.Add("Z", 3);                              // 첫 번째 슬롯 갯수
-        itemCount.Add("X", 3);                              // 두 번째 슬롯 갯수
-        itemCount.Add("C", 0);                              // 세 번째 슬롯 갯수
 
         stageNum = 0;
         isWarp = true;
@@ -145,19 +144,23 @@ public class PlayerController : MonoBehaviour
         uIHealthBar.SetCurrentHealth(currentHealth);                
         uIManaBar.SetCurrentMana(currentMana);
 
-        itemCount_01.text = itemCount["Z"].ToString();
-        itemCount_02.text = itemCount["X"].ToString();
-        itemCount_03.text = itemCount["C"].ToString();
+        slotText_01.text = healthPotionCount.ToString();
+        slotText_02.text = manaPotionCount.ToString();
+        slotText_03.text = gameSystem.skillList[currentSkillNumber][3];
 
         essenceCountText.text = essenceCount.ToString();
 
         stageNumText.text = stageNum.ToString();
+
+        slotSkillImage.sprite = Resources.Load<Sprite>("Sprites/Skills/" + skillCondition[currentSkillNumber].ToString());
+
     }
 
     /// <summary>
     /// 스킬 사용에 관한 함수
     /// 평상시에는 isPlayerAttack가 false로 설정되어 있으나 스킬을 사용할 경우 true로 변한다.
     /// 또한 각 스킬에 맞는 딜레이 시간이 지나면 다시 false로 설정한다.
+    /// 추가로 마우스 휠을 움직임에 따라 currentSkillNumber 값이 증가하며 최대치에 도달하면 다시 0으로 초기화된다.
     /// </summary>
     private void UseSkill()
     {
@@ -167,16 +170,31 @@ public class PlayerController : MonoBehaviour
             delaySkill -= Time.deltaTime;
             isPlayerAttack = true;
         }
-        if (Input.GetKeyDown(KeyCode.Q) && delaySkill <= 0)        // 화염구 스킬 사용
+        if(Input.GetKeyDown(KeyCode.C) && delaySkill <= 0)
         {
-            Instantiate(FireBall, transform.position + new Vector3(lastMove.x / 2, lastMove.y / 2, 0f), transform.rotation);
+            switch (skillCondition[currentSkillNumber])
+            {
+                case 0:
+                    Instantiate(FireBall, transform.position + new Vector3(lastMove.x / 2, lastMove.y / 2, 0f), transform.rotation);
+                    break;
+                case 1:
+                    Vector3 targetPos = camera.ScreenToWorldPoint(Input.mousePosition);
+                    targetPos.z = 0;
+                    Instantiate(Lightning, targetPos, transform.rotation);
+                    break;
+            }
         }
-        if (Input.GetKeyDown(KeyCode.E) && delaySkill <= 0)       // 번개작렬 스킬 사용
+        int maxSkillNumber = skillCondition.Count;
+        currentSkillNumber += Mathf.RoundToInt(Input.GetAxis("Mouse ScrollWheel") * 10);
+        if(currentSkillNumber >= maxSkillNumber)
         {
-            Vector3 targetPos = camera.ScreenToWorldPoint(Input.mousePosition);
-            targetPos.z = 0;
-            Instantiate(Lightning, targetPos, transform.rotation);
+            currentSkillNumber = 0;
         }
+        if(currentSkillNumber < 0)
+        {
+            currentSkillNumber = maxSkillNumber - 1;
+        }
+
     }
 
     /// <summary>
@@ -221,46 +239,18 @@ public class PlayerController : MonoBehaviour
     /// </summary>
     private void UseItem()
     {
-        if (Input.GetKeyDown(KeyCode.Z) && itemCount["Z"] > 0)
+        if (Input.GetKeyDown(KeyCode.Z) && healthPotionCount > 0)
         {
-            int itemCode = itemSlot["Z"];
-            itemCount["Z"] -= 1;
-            CheckItemType(itemCode);
+            healthPotionCount -= 1;
+            currentHealth += healthPotionPower;
         }
-        if (Input.GetKeyDown(KeyCode.X) && itemCount["X"] > 0)
+        if (Input.GetKeyDown(KeyCode.X) && manaPotionCount > 0)
         {
-            int itemCode = itemSlot["X"];
-            itemCount["X"] -= 1;
-            CheckItemType(itemCode);
-        }
-        if (Input.GetKeyDown(KeyCode.C) && itemCount["C"] > 0)
-        {
-            int itemCode = itemSlot["C"];
-            itemCount["C"] -= 1;
-            CheckItemType(itemCode);
+            manaPotionCount -= 1;
+            currentMana += manaPotionPower;
         }
     }
 
-    /// <summary>
-    /// 사용한 아이템 종류 확인 함수
-    /// 어떤 종류의 아이템을 사용했는지 확인하고 그에 맞는 효과를 부여한다.
-    /// </summary>
-    /// <param name="itemCode">사용한 아이템 코드</param>
-    private void CheckItemType(int itemCode)
-    {
-        switch (gameSystem.itemList[itemCode][2])
-        {
-            case "health":
-                currentHealth += int.Parse(gameSystem.itemList[itemCode][3]);
-                break;
-            case "mana":
-                currentMana += int.Parse(gameSystem.itemList[itemCode][3]);
-                break;
-            default:
-                Debug.Log("no item");
-                break;
-        }
-    }
 
     public void ResetPosition()
     {
@@ -327,10 +317,6 @@ public class PlayerController : MonoBehaviour
         {
             Destroy(collision.gameObject);
             essenceCount += 1;
-        }
-        if (collision.tag.Equals("ItemBox"))
-        {
-
         }
     }
 }
--- RoguelikeUnity/Assets/Scripts/UI/UIItemBox.cs
+++ RoguelikeUnity/Assets/Scripts/UI/UIItemBox.cs
@@ -8,16 +8,20 @@ public class UIItemBox : MonoBehaviour
     private GameObject canvas;                      // 캔버스
     public GameObject itemBoxObj;                   // 아이템 박스 UI 오브젝트
     RectTransform itemBoxUI;                        // 캔버스에서의 아이템 박스 UI
-    private TextMeshProUGUI essenceCount;           // 아이템 박스에 표기할 정수 갯수 
+    int essenceCount;
+    private TextMeshProUGUI essenceCountText;           // 아이템 박스에 표기할 정수 갯수 
+    private PlayerController playerController;      // 플레이어 컨트롤러 클래스
     // Start is called before the first frame update
     void Start()
     {
         canvas = GameObject.Find("Canvas");
         itemBoxUI = Instantiate(itemBoxObj, canvas.transform).GetComponent<RectTransform>();
-        essenceCount = itemBoxUI.GetChild(0).GetComponent<TextMeshProUGUI>();
-        essenceCount.text = Random.Range(1, 5).ToString();
+        essenceCountText = itemBoxUI.GetChild(0).GetComponent<TextMeshProUGUI>();
+        essenceCount = Random.Range(1, 5);
+        essenceCountText.text = essenceCount.ToString();
         Vector3 itemBoxPos = Camera.main.WorldToScreenPoint(new Vector3(transform.position.x, transform.position.y - 0.75f, 0f));
         itemBoxUI.position = itemBoxPos;
+        playerController = GameObject.Find("Player").GetComponent<PlayerController>();
     }
 
     // Update is called once per frame
@@ -27,6 +31,23 @@ public class UIItemBox : MonoBehaviour
     }
     private void OnDestroy()
     {
-        Destroy(itemBoxUI.gameObject);
+        try
+        {
+            Destroy(itemBoxUI.gameObject);
+        }
+        catch
+        {
+            Debug.Log("Error while destoy the itemBox UI.");
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.name == "Player")
+        {
+            if(playerController.essenceCount >= essenceCount)
+            {
+                Destroy(this.gameObject);
+            }
+        }
     }
 }
